2037e0bc7aea2d20a677b69bf12fb04f40457de6
ahmeddhamddyy <medo3_200162@yahoo.com>
2024-05-24 21:56:50 +0200

    No Node   Modules

--- app.js
+++ app.js
@@ -20,12 +20,8 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Hello world!"));
 
 app.use("/api/users", require("./routes/userRoute"));
-
-
 app.use("/api/location", require("./routes/locationRoute")); // Add this line
 app.use("/api/upload", require("./routes/multerRoute")); // Mount MulterRoute to handle file uploads
 app.use("/api/users",require("./routes/bankRoute"),validateToken);
 
-const port = 9090;
 
-app.listen(9090, () => console.log(`Server running on port ${port}`));

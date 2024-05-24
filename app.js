f00230f3a299768a7bf45a7d537f8ad755ef6817
MaiElaser <maielaser22@gmail.com>
2024-05-24 16:34:00 +0300

    Error

--- app.js
+++ app.js
@@ -7,16 +7,11 @@ const dotenv = require("dotenv").config();
 const nodemailer = require("nodemailer");
 const validateToken = require("./middleware/validateTokenHandler");
 const app = express();
-const { sendOtp } = require("./utils/otp");
-const bodyParser = require('body-parser');
-const User = require('./models/userModel'); // Adjust the path as necessary
+
 
 // Connect Database
 connectDB();
 
-// Use body-parser middleware to parse JSON requests//new
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 //old
 app.use(express.json());
@@ -25,10 +20,7 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Hello world!"));
 
 app.use("/api/users", require("./routes/userRoute"));
-// Error handling middleware//new
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-  });
+
 
 app.use("/api/location", require("./routes/locationRoute")); // Add this line
 app.use("/api/upload", require("./routes/multerRoute")); // Mount MulterRoute to handle file uploads
